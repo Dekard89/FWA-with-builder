3239f4f6074041f13f6798a1bef6498cc5d0b597
Дмитрий Насенков <dmitry_nk@outlook.com>
2023-12-18 20:37:00 +0300

    added implementation and fixed bugs

---
+++ Bl.Model/RecipeRepoModel.cs
@@ -0,0 +1,34 @@
+﻿using Domain;
+using RecipeRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bl.Model
+{
+    public class RecipeRepoModel : IRecipeRepoModel
+    {
+        public IRepository
+        public Recipe Create(string name, TimeSpan timeToCook, string discreption=null)
+        {
+            return new Recipe { Name = name, TimeToCook = timeToCook, Description = discreption };
+        }
+
+        public Recipe FindByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdaeDescription(string description)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateTimeToCook(TimeSpan timeToCook)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
