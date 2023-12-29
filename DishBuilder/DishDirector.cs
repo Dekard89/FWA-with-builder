86b33063db6a4eabc131aa9aa97ad4c46a6dddd7
Дмитрий Насенков <dmitry_nk@outlook.com>
2023-12-29 21:00:14 +0300

    director

--- DishBuilder/DishDirector.cs
+++ DishBuilder/DishDirector.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DishBuilder;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,38 @@ using System.Threading.Tasks;
 
 namespace BL.BuiderForDish
 {
-    internal class DishDirector
+     public class DishDirector
     {
+        private readonly IDishBuider _dishBuider;
+
+        public DishDirector(IDishBuider dishBuider)
+        {
+            _dishBuider = dishBuider;
+        }
+        public Dish CookWihoutTopings()
+        {
+            _dishBuider.BuidPrice().BuidOther();
+
+            return _dishBuider.GetDish();
+        }
+        public Dish CookWithFirstToping()
+        {
+            _dishBuider.BuidPrice().BuidOther().AppendFirstToping();
+
+            return _dishBuider.GetDish();
+        }
+        public Dish CookWithSecondToping()
+        {
+            _dishBuider.BuidPrice().BuidOther().AppenSecondToping();
+
+            return _dishBuider.GetDish();
+        }
+        public Dish CookWithTwoTopings()
+        {
+            _dishBuider.BuidPrice().BuidOther().AppendFirstToping().AppendFirstToping();
+
+            return _dishBuider.GetDish();
+        }
+
     }
 }
